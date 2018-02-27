21db9092337c1316df41802ffef6e384687c7d7d
Ignasi <Ignasi@DESKTOP-P3T9DUI>
2018-02-27 10:02:35 +0100

    examen Entorns

--- examen_LopezCasas/src/examen_LopezCasas/examen_LopezCasas.java
+++ examen_LopezCasas/src/examen_LopezCasas/examen_LopezCasas.java
@@ -5,16 +5,12 @@ public class examen_LopezCasas
 	public static void main (String[] args) {
 		printOne();
 		printOne();
-		printTwo();
+		
 	}
 	public static void printOne() {
 		System.out.println("Hello World");
 		
 	}
-	public static void printTwo() {
-		printOne();
-		printOne();
-		
-	}
+	
 
 }
